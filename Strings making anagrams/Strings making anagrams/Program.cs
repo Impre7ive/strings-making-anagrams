e9edf7a82c127e347d8036f50d5f70551a32bfbd
Dmitry Liashenko <adslyashenko@gmail.com>
2021-09-22 21:03:48 +0300

    Short refactoring: renamed and divided anagram method.

--- Strings making anagrams/Strings making anagrams/Program.cs
+++ Strings making anagrams/Strings making anagrams/Program.cs
@@ -3,27 +3,31 @@ using System.Linq;
 
 namespace Strings_making_anagrams {
     class Program {
-        public static int MakeAnagram(string a, string b) {
-            if (a.Length == 0 || b.Length == 0)
-                return -1;
-
+        public static char[] ExtractAnagram(string a, string b) {
             char[] firstStr = a.ToArray<char>();
             char[] secondStr = b.ToArray<char>();
             //Anagram == Intersection
             //In the cycle compare each symbol, when it's found - remove it from target array and continue 
-            char[] stringIntersection = firstStr.Intersect<char>(secondStr).ToArray();
+            return firstStr.Intersect<char>(secondStr).ToArray();
+        }
 
-            int result = firstStr.Length + secondStr.Length - 2 * stringIntersection.Length;
+        public static int RetrieveMinimalDeletionToGetAnagram(char[] anagram, string a, string b) {
+            if (anagram.Length == 0)
+                return -1;
+
+            int result = a.Length + b.Length - 2 * anagram.Length;
 
             //Count number of deletion
-            return result > firstStr.Length ? -1 : result;
+            return (result > a.Length && result > b.Length) ? -1 : result;
         }
 
         static void Main(string[] args) {
             string a = Console.ReadLine();
             string b = Console.ReadLine();
 
-            int minSymbolsForAnagram = MakeAnagram(a, b);
+            char[] anagram = ExtractAnagram(a, b);
+
+            int minSymbolsForAnagram = RetrieveMinimalDeletionToGetAnagram(anagram, a, b);
 
             Console.WriteLine($"Minimal number of removed symbols for an anagram creation is {minSymbolsForAnagram}");
             Console.ReadLine();
